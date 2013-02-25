5fe31339bf12b632caeecafdd42a1d0b5ccbfcd3
Vernon Jones <vernonrjones@gmail.com>
2013-02-24 17:41:37 -0800

    Refined Device IP structure

--- src/router/routing/routing.c
+++ src/router/routing/routing.c
@@ -1,13 +1,16 @@
+#define _POSIX_C_SOURCE 200809L		// Required for nanosleep()
+
 #include <stdint.h>
 #include <time.h>
 
 #include "routing.h"
 
 
-struct RoutingTable {
-	uint32_t ip[4];
-	struct timespec clkdiff;
+struct DeviceIP {
+	uint32_t ip[4];		// IP address
+	uint32_t clkdiff;	// ping offset
 };
 
-uint32_t *routing_table[ROUTING_ARRAYSIZE];
+struct DeviceIP *routing_table[ROUTING_ARRAYSIZE];
+//uint32_t *routing_table[ROUTING_ARRAYSIZE];
 
