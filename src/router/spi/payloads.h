356f58d6273e7f2bd3af099d13bcde15b8491562
Vernon Jones <vernonrjones@gmail.com>
2013-03-02 14:48:22 -0800

    Added hatching type, added enums for types
    Hatching type has been added: router statically defines IP
    Enums have been added for data types

--- src/router/spi/payloads.h
+++ src/router/spi/payloads.h
@@ -30,6 +30,19 @@
 	uint8_t serialdata[sizeof(type)]; \
 }
 
+
+
+struct SansgridHatching {
+	// Datatype: 0xF1
+	uint8_t datatype;
+	uint8_t ip[16];
+	uint8_t padding[64];
+};
+
+SANSGRID_UNION(struct SansgridHatching, SansgridHatchingConv);
+
+
+
 struct SansgridFly {
 	// Datatype: 0xF0
 	uint8_t datatype;
@@ -161,12 +174,55 @@ struct SansgridChirp {
 	// 		0x22: Start of data stream
 	// 		0x23: Continued data stream
 	// 		0x24: End of data stream
+	// 		0x25: Network Disconnecting Sensor
+	// 		0x26: Sensor Disconnecting from Network
 	uint8_t datatype;
+	uint8_t datasize;	// Used to truncate data
 	uint8_t data[80];
 };
 
 SANSGRID_UNION(struct SansgridChirp, SansgridChirpConv);
 
+
+
+enum SansgridDataTypeEnum {
+	// Initialization packets
+	SG_HATCH = 0xF1,
+	SG_FLY = 0xF0,
+	// Eyeballing
+	SG_EYEBALL = 0x00,
+	// Pecking
+	SG_PECK = 0x01,
+	// Singing
+	SG_SING_WITH_KEY = 0x02,
+	SG_SING_WITHOUT_KEY = 0x03,
+	// Mocking
+	SG_MOCK_WITH_KEY = 0x07,
+	SG_MOCK_WITHOUT_KEY = 0x08,
+	// Peacocking
+	SG_PEACOCK = 0x0C,
+	// Nesting
+	SG_NEST = 0x10,
+	// Squawking
+	SG_SQUAWK_SERVER_CHALLENGE_SENSOR = 0x11,
+	SG_SQUAWK_SENSOR_RESPOND_NO_REQUIRE_CHALLENGE = 0x15,
+	SG_SQUAWK_SENSOR_RESPOND_REQUIRE_CHALLENGE = 0x16,
+	SG_SQUAWK_SENSOR_CHALLENGE_SERVER = 0x17,
+	SG_SQUAWK_SERVER_DENY_SENSOR = 0x1B,
+	SG_SQUAWK_SERVER_RESPOND = 0x1C,
+	SG_SQUAWK_SENSOR_ACCEPT_RESPONSE = 0x1D,
+	// Heartbeat
+	SG_HEARTBEAT_ROUTER_TO_SENSOR = 0x1E,
+	SG_HEARTBEAT_SENSOR_TO_ROUTER = 0x1F,
+	// Chirp
+	SG_CHIRP_COMMAND_SERVER_TO_SENSOR = 0x20,
+	SG_CHIRP_DATA_SENSOR_TO_SERVER = 0x21,
+	SG_CHIRP_DATA_STREAM_START = 0x22,
+	SG_CHIRP_DATA_STREAM_CONTINUE = 0x23,
+	SG_CHIRP_DATA_STREAM_END = 0x24,
+	SG_CHIRP_NETWORK_DISCONNECTS_SENSOR = 0x25,
+	SG_CHIRP_SENSOR_DISCONNECT = 0x26
+};
 
 #undef SANSGRID_UNION
 // vim: ft=c ts=4 noet sw=4:
