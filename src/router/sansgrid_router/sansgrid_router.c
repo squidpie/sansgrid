99b0e60fa82dc2db5b511e4dc60b5c88a8434267
Vernon Jones <vernonrjones@gmail.com>
2013-03-23 14:20:57 -0700

    These should have been added earlier

---
+++ src/router/sansgrid_router/sansgrid_router.c
@@ -0,0 +1,140 @@
+/* Router Starting Point
+ *
+ * Copyright (C) 2013 SansGrid
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ *
+ */
+
+#include <stdio.h>
+#include <pthread.h>
+#include <stdint.h>
+#include "sansgrid_router.h"
+#include "../../payloads.h"
+#include "../dt_handlers/handlers.h"
+#include "../../sg_serial.h"
+
+
+void *dispatchRuntime(void *arg) {
+	uint8_t *serial_data;
+	volatile int i = 0;
+
+	while (1) {
+		if (queueDequeue(dispatch, &serial_data) == -1) 
+			exit(EXIT_FAILURE);
+		switch (serial_data[0]) {
+			case SG_HATCH:
+				routerHandleHatching(routing_table, serial_data);
+				break;
+			case SG_FLY:
+				routerHandleFly(routing_table, serial_data);
+				break;
+			case SG_EYEBALL:
+				routerHandleEyeball(routing_table, serial_data);
+				break;
+			case SG_PECK:
+				routerHandlePeck(routing_table, serial_data);
+				break;
+			case SG_SING_WITH_KEY:
+			case SG_SING_WITHOUT_KEY:
+				routerHandleSing(routing_table, serial_data);
+				break;
+			case SG_MOCK_WITH_KEY:
+			case SG_MOCK_WITHOUT_KEY:
+				routerHandleMock(routing_table, serial_data);
+				break;
+			case SG_PEACOCK:
+				routerHandlePeacock(routing_table, serial_data);
+				break;
+			case SG_NEST:
+				routerHandleNest(routing_table, serial_data);
+				break;
+			case SG_SQUAWK_SERVER_CHALLENGE_SENSOR:
+			case SG_SQUAWK_SENSOR_RESPOND_NO_REQUIRE_CHALLENGE:
+			case SG_SQUAWK_SENSOR_RESPOND_REQUIRE_CHALLENGE:
+			case SG_SQUAWK_SENSOR_CHALLENGE_SERVER:
+			case SG_SQUAWK_SERVER_DENY_SENSOR:
+			case SG_SQUAWK_SERVER_RESPOND:
+			case SG_SQUAWK_SENSOR_ACCEPT_RESPONSE:
+				routerHandleSquawk(routing_table, serial_data);
+				break;
+			case SG_HEARTBEAT_ROUTER_TO_SENSOR:
+			case SG_HEARTBEAT_SENSOR_TO_ROUTER:
+				routerHandleHeartbeat(routing_table, serial_data);
+				break;
+			case SG_CHIRP_COMMAND_SERVER_TO_SENSOR:
+			case SG_CHIRP_DATA_SENSOR_TO_SERVER:
+			case SG_CHIRP_DATA_STREAM_START:
+			case SG_CHIRP_DATA_STREAM_CONTINUE:
+			case SG_CHIRP_DATA_STREAM_END:
+			case SG_CHIRP_NETWORK_DISCONNECTS_SENSOR:
+			case SG_CHIRP_SENSOR_DISCONNECT:
+				routerHandleChirp(routing_table, serial_data);
+				break;
+			default:
+				break;
+		}
+	}
+	pthread_exit(arg);
+}
+
+void *spiReaderRuntime(void *arg) {
+	// Read from SPI and queue data onto dispatch
+	uint8_t *serial_data;
+	uint32_t size;
+	while (1) {
+		while (sgSerialReceive(&serial_data, &size) == -1) {
+			sched_yield();
+		}
+		queueEnqueue(dispatch, serial_data);
+	}
+	pthread_exit(arg);
+}
+
+
+void fnExit(void) {
+	printf("Exiting\n");
+}
+
+
+int sg_router_runtime(void) {
+	pthread_t 	serial_read_thread,
+				dispatch_thread;
+
+	atexit(fnExit);
+	dispatch = queueInit(200);
+	routing_table = routingTableInit(router_base);
+	void *arg;
+
+
+	pthread_create(&serial_read_thread, NULL, spiReaderRuntime, dispatch);
+	pthread_create(&dispatch_thread, NULL, dispatchRuntime, dispatch);
+
+
+	while (1) {
+		sched_yield();
+	}
+
+
+	pthread_join(serial_read_thread, &arg);
+	pthread_join(dispatch_thread, &arg);
+
+	queueDestroy(dispatch);
+	routingTableDestroy(routing_table);
+	return 0;
+}
+
+// vim: ft=c ts=4 noet sw=4:
