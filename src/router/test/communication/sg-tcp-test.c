5ffbf8ae70036babbc69d6b4d393c05d18d7feed
Vernon Jones <vernonrjones@gmail.com>
2013-03-23 19:01:06 -0700

    Added tcp stubs for testing

---
+++ src/router/test/communication/sg-tcp-test.c
@@ -0,0 +1,87 @@
+/* TCP read/write stubs
+ *
+ * Copyright (C) 2013 SansGrid
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <pthread.h>
+#include <string.h>
+#include "../../../sg_serial.h"
+#include "../../communication/sg_tcp.h"
+
+static FILE *FPTR_TCP_WRITE = NULL,
+			*FPTR_TCP_READ = NULL;
+
+
+void sgTCPTestSetReader(FILE *FPTR) {
+	FPTR_TCP_READ = FPTR;
+}
+
+void sgTCPTestSetWriter(FILE *FPTR) {
+	FPTR_TCP_WRITE = FPTR;
+}
+
+
+
+int8_t sgTCPSend(SansgridSerial *sg_serial, uint32_t size) {
+	// Send size bytes of serialdata
+	int i;
+	SANSGRID_UNION(SansgridSerial, SGSU) sg_serial_union;
+
+
+	if (FPTR_TCP_WRITE == NULL)
+		return -1;
+
+	sg_serial_union.formdata = sg_serial;
+	
+	for (i=0; i<sizeof(SansgridSerial); i++) {
+		putc(sg_serial_union.serialdata[i], FPTR_TCP_WRITE);
+	}
+	
+	return 0;
+}
+
+
+
+int8_t sgTCPReceive(SansgridSerial **sg_serial, uint32_t *size) {
+	// Receive serialdata, size of packet stored in size
+	int timeout = 0;
+	char lptr[sizeof(SansgridSerial)+1];
+
+	if (FPTR_TCP_READ == NULL)
+		return -1;
+
+	// Read from the pipe 
+	while (fgets(lptr, sizeof(SansgridSerial)+1, FPTR_TCP_READ) == NULL) {
+		timeout++;
+		if (timeout > 10000) {
+			return -1;
+		}
+		sched_yield();
+	}
+	*sg_serial = (SansgridSerial*)malloc(sizeof(SansgridSerial));
+	memcpy(*sg_serial, lptr, sizeof(SansgridSerial));
+	*size = sizeof(SansgridSerial);
+	return 0;
+}
+
+
+
+// vim: ft=c ts=4 noet sw=4:
+
