2848e778bab2420ab643db4f4ad3f898f921699e
kane <kane@mrbun.(none)>
2013-03-28 17:02:01 -0700

    Intermediate save.  Eyeballing work still in progress.

---
+++ src/router/router_to_server/config.pl
@@ -0,0 +1,8 @@
+# NOTE: this file is not intended to be executed directly.
+
+# Server key has determined by server 
+key 	=> '0178931dc6d0d5c2938bc228a41972f4d188787d68b982dfeedafb949bfaaafd',
+
+# Server address (IP address or qualified name)
+url		=> '10.42.0.40',
+
---
+++ src/server/www/pages/aa_style.php
@@ -0,0 +1,7 @@
+<?
+
+function get_stylesheet () {
+
+}
+
+?>
