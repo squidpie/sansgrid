c19e41654a25a0e63dbaa4bd0b57a2cd4b7af6cd
Matthew Branstetter <branstem@cecs.pdx.edu>
2013-04-20 14:28:25 -0700

    Modified sgSerial.h and added sgSerial.cpp and sgSerial_due.h. All files are now architecture dependent with a macro _MCU_ iether _ARC_DUE_ or _ARC_PI_

---
+++ src/sensor/Master/sgSerial.h
@@ -0,0 +1,79 @@
+/* Serial Communication API
+ *
+ * Copyright (C) 2013 SansGrid
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ *
+ */
+
+#ifndef __SG_SERIAL_H__
+#define __SG_SERIAL_H__
+
+// Set up a macro depending on architecture
+#ifdef __MCU__
+#define ARCH_DUE
+#include <Arduino.h>
+#include "sgSerial_due.h"
+#else
+#define ARCH_PI
+#include <stdint.h>
+#include "sgSerial_pi.h"
+#include "payloads.h"
+#endif //_MCU_
+
+#define SLAVE_SELECT  10        // SS pin 10
+#define SLAVE_READY  9          // Hand shake pin identifying Slave has data to send
+#define NUM_BYTES 98			// Number of bytes sent or received over SPI per packet
+#define CONTROL 1				// Number of bytes in control 
+#define IP_ADDRESS 16			// Number of bytes in source/destination IP address
+#define PAYLOAD 81				// Number of bytes in payload 
+#define RECEIVE 0x00			// Control byte for receiving data
+#define TRANSMIT 0x01			// Control byte for transmit data
+
+typedef struct {
+	uint8_t control;				// control
+	uint8_t ip_addr[IP_SIZE];		// Overloaded IP Field
+									// contains origin or destination IP address
+	uint8_t payload[81];			// payload
+} SansgridSerial;
+
+// Opens serial device for reading/writing, configures ports, sets order data bits 
+// are shifted in as MSB or LSB, and sets the clock frequency. Function is called 
+// prior to sending or receiving any data over the serial line. 
+int8_t sgSerialOpen(void);
+// Configure radio as a router radio
+// Radio will be configured as a sensor radio by default.
+// Sets radio as a "router radio" 
+int8_t sgSerialSetAsRouter(void);
+// Sets radio as a "sensor radio".
+int8_t sgSerialSetAsSensor(void);
+// Transmit number of bytes "size" from SansgridSerial structure to Slave over SPI. 
+// For full duplex a second SansgridSerial is passed in with a second size integer 
+// to receive data from the slave in return. The most significant bit first in 
+// Big Endian byte order. Number of bytes is not to exceed control byte plus 
+// IP address destination/source bytes plus payload bytes. 
+int8_t sgSerialSend(SansgridSerial *tx, SansgridSerial **rx, uint32_t size);
+// Receive number of bytes "size" and places in SansgridSerial structure. The most 
+// significant bit first in Big Endian byte order. Number of bytes is not to exceed 
+// control byte plus IP address destination/source bytes plus payload bytes. 
+int8_t sgSerialReceive(SansgridSerial **rx, uint32_t size);
+// Function is called when all SPI input and output is completed. Stops SPI from being 
+// transmitted and received. 
+int8_t sgSerialClose(void);
+
+#endif // __SG_SERIAL_H__
+
+// vim: ft=c ts=4 noet sw=4:
