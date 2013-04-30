886ea281fa5291c34ece7cbf4271d7ad178bc52f
Matthew Branstetter <branstem@cecs.pdx.edu>
2013-04-29 18:01:13 -0700

    sensorParse is completed, will test later this evening. Need to refine payloadHandler, then test and sensor should be ready for integration.

--- src/sensor/sensorParse/sensorParse.cpp
+++ src/sensor/sensorParse/sensorParse.cpp
@@ -29,29 +29,40 @@
 
 // Payloads recieved at Sensor from Router
 void parseFly( SansgridSerial *rx , SansgridFly *sg_fly ){
-	// TBD
+	memcpy( sg_fly->dt , rx->payload , DT );
+	memcpy( sg_fly->network_name , rx->payload + 1 , DATA );
 }
 
 void parsePeck( SansgridSerial *rx , SansgridPeck *sg_peck ){
-	// TBD
+	memcpy( sg_peck->dt , rx->payload , DT );
+	memcpy( sg_peck->router_ip , rx->payload + DT , IP_ADDRESS );
+	memcpy( sg_peck->ip_address , rx->payload + DT + IP_ADDRESS , IP_ADDRESS );
+	memcpy( sg_peck->server_id , rx->payload + DT + IP_ADDRESS + IP_ADDRESS , SERVER_ID );
+	memcpy( sg_peck->recognition , rx->payload + DT + IP_ADDRESS + IP_ADDRESS + SERVER_ID , RECOGNITION ); 
+	memcpy( sg_peck->manid , rx->payload + DT + IP_ADDRESS + IP_ADDRESS + SERVER_ID + RECOGNITION , MANID );
+	memcpy( sg_peck->modnum, rx->payload + DT + IP_ADDRESS + IP_ADDRESS + SERVER_ID + RECOGNITION + MANID , MODNUM );
+	memcpy( sg_peck->sn , rx->payload + DT + IP_ADDRESS + IP_ADDRESS + SERVER_ID + RECOGNITION + MANID + MODNUM , SN );
 }
 
 void parseSing( SansgridSerial *rx , SansgridSing *sg_sing ){
-	// TBD
+	memcpy( sg_sing->dt , rx->payload , DT );
+	memcpy( sg_sing->server_public_key , rx->payload + DT , SERVER_KEY );
 }
 
 void parseSquawk( SansgridSerial *rx , SansgridSquawk *sg_squawk ){
-	// TBD
+	memcpy( sg_squawk->dt , rx->payload , DT );
+	memcpy( sg_squawk->data , rx->payload + DT , DATA );
 }
 
 void parseNest( SansgridSerial *rx , SansgridNest *sg_nest ){
-	// TBD
+	memcpy( sg_nest->dt , rx->payload , DT );
 }
 
 void parseHeartbeat( SansgridSerial *rx , SansgridHeartbeat *sg_heartbeat ){
-    // TBD
+    memcpy( sg_heartbeat->dt , rx->payload , DT );
 }
 
 void parseChirp( SansgridSerial *rx , SansgridChirp *sg_chirp ){
-    // TBD
+    memcpy( sg_chirp->dt , rx->payload , DT );
+	memcpy( sg_chirp->data , rx->payload + DT , DATA );
 }
