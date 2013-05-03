a9ff9174a53386a7713a44f7bb3abea802ff02fd
Vernon Jones <vernonrjones@gmail.com>
2013-05-03 12:47:12 -0700

    Took important stuff from spitest-arduino

--- src/router/sansgridrouter/communication/sg_serial_rpi.c
+++ src/router/sansgridrouter/communication/sg_serial_rpi.c
@@ -38,117 +38,67 @@
 #define MHZ(freq) (1000*KHZ(freq))
 
 // EEPROM-specific defines
-#define SPI_SPEED_MHZ	2
-#define WRITE 		0x02
-#define READ		0x03
-#define WRITE_ENABLE 	0x06
-#define WRITE_CYCLE	5000
+#define SPI_SPEED_KHZ	500
+#define WRITE_CYCLE		1
+#define WRITE_MAX_BYTES 1
 
-//int spiWrite(char *buffer, uint16_t address, int size) {
-int spiWrite(sgSerial *sg_serial, int size) {
+
+ssize_t write_noconst(int fd, void *buf, size_t count)
+{
+	return write(fd, buf, count);
+}
+
+
+int spiTransfer(char *buffer, int size, ssize_t(*op)(int, void*, size_t))
+{
 	int i;
 	int fd;
 	// only 32 bytes can be written at a time; see below
-	int bounded_size = (size > 32 ? 32 : size);
+	int bounded_size = (size > WRITE_MAX_BYTES ? WRITE_MAX_BYTES : size);
 	// prepend the command and address to the data
-	char newbuffer[bounded_size+3];
+	//char newbuffer[bounded_size];
 
-	// Set up SPI
-	if ((fd = wiringPiSPISetup (0, MHZ(SPI_SPEED_MHZ))) < 0)
-		fprintf(stderr, "SPI Setup failed: %s\n", strerror (errno));
 
-	// Have to make room for command and address
-	for (i=3; i<bounded_size+3; i++)
-		newbuffer[i] = buffer[i-3];
 
-	// Allow writes to the EEPROM
-	// Has to be done before every write cycle
-	newbuffer[0] = WRITE_ENABLE;
-	write(fd, newbuffer, 1);
-
-	// Write to specified address
-	newbuffer[0] = WRITE;
-	newbuffer[1] = address >> 8;
-	newbuffer[2] = address & 0xff;
-
-	write(fd, newbuffer, bounded_size+3);
+	wiringPiSPIDataRW(0, buffer, bounded_size);
 	// Wait for the write to cycle
 	usleep(WRITE_CYCLE);
-	close(fd);
-	if (size > 32) {
+	if (size > WRITE_MAX_BYTES) {
 		// Only one page (of 32 bytes) can be written
 		// at a time. If more than 32 bytes are being written,
 		// break the line into multiple pages
-		spiWrite(&buffer[32], address+0x0020, size-32);
+		spiTransfer(&buffer[WRITE_MAX_BYTES], size-WRITE_MAX_BYTES, op);
 	}
 
 	return 0;
 }
 
 
-int spiRead(char *buffer, uint16_t address, int size) {
-	// Read from an EEPROM chip over SPI
-	int fd;
-	char newbuffer[size+3];
-	int i;
 
-	// Set up reading from EEPROM
+int8_t sgSerialSend(SansgridSerial *sg_serial, uint32_t size) {
+	// Send size bytes of serialdata
+	int fd;
+	char buffer[size+1];
+
+	// Set up SPI
 	if ((fd = wiringPiSPISetup (0, MHZ(SPI_SPEED_MHZ))) < 0)
 		fprintf(stderr, "SPI Setup failed: %s\n", strerror (errno));
 
-	// Prepend command and address to buffer
-	newbuffer[0] = READ;
-	newbuffer[1] = address >> 8;
-	newbuffer[2] = address & 0xff;
+	memcpy(buffer, sg_serial, size);
+	spiTransfer(buffer, size, write_noconst);
 
-	// Send command/address, read data
-	wiringPiSPIDataRW(0, newbuffer, size+3);
-
-	// shift the command and address out of buffer
-	for (i=0; i<size; i++)
-		buffer[i] = newbuffer[i+3];
-	// finish up
 	close(fd);
 
 	return 0;
 }
 
-int8_t sgSerialSend(SansgridSerial *sg_serial, uint32_t size) {
-	// Send size bytes of serialdata
-	return -1;
-}
 
 int8_t sgSerialReceive(SansgridSerial **sg_serial, uint32_t *size) {
 	// Receive serialdata, size of packet stored in size
+	// TODO: Get code from
+	// https://git.drogon.net/?p=wiringPi;a=blob;f=examples/isr.c;h=2bef54af13a60b95ad87fbfc67d2961722eb016e;hb=HEAD
 	return -1;
 }
-
-/*
-// EEPROM-Specific main function
-int main(void) {
-	int i;
-	int fd;
-	char buffer[70];
-
-	// Writing to the EEPROM chip
-	snprintf(buffer, 64, "This is a much longer sentence. It is bigger than 32.");
-	spiWrite(buffer, 0x0000, 64);
-
-	snprintf(buffer, 32, "Well I'll be!");
-	spiWrite(buffer, 0x0040, 32);
-
-
-	// Read using wraparound
-	spiRead(buffer, 0x0000, 64);
-	printf("%s\n", buffer);
-
-	// Read the second datum
-	spiRead(buffer, 0x0040, 32);
-	printf("%s\n", buffer);
-
-	return 0;
-}
-*/
 
 //#else
 //#error "Expected Raspberry Pi Architecture Definition"
