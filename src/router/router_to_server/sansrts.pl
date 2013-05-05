c309cd78082ef72c5a2ced4e1a9b4eb09ea38f1e
kane <kane@mrbun.(none)>
2013-05-04 19:21:35 -0700

    Debugging with Vern, changed stuff. Don't ask questions.

--- src/router/router_to_server/sansrts.pl
+++ src/router/router_to_server/sansrts.pl
@@ -12,6 +12,8 @@ my $server 	= $config{'url'} or die ("Error in config.pl, can't read server URL.
 # sansrts.pl requires a single command-line parameter, the payload
 die "Usage: sansrts.pl DATA\n\n" if (scalar(@ARGV) != 1);
 my $payload = shift(@ARGV);
+
+#`echo curl -s $server/API.php --data-urlencode key='$key' --data-urlencode payload='$payload' > ./last_data_sent.text`;
 	
 my $reply = `curl -s $server/API.php --data-urlencode key='$key' --data-urlencode payload='$payload'`;
 
