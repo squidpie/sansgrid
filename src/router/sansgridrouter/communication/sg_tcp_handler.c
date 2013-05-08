f6b174961829ffad28e9f2094bab8537640f6d10
Vernon Jones <vernonrjones@gmail.com>
2013-05-08 13:32:39 -0700

    rdid is now separated from IP address on router

--- src/router/sansgridrouter/communication/sg_tcp_handler.c
+++ src/router/sansgridrouter/communication/sg_tcp_handler.c
@@ -319,9 +319,10 @@ int8_t sgServerToRouterConvert(char *payload, SansgridSerial *sg_serial) {
 	rdid_size = (strlen(match(dict, size, "rdid"))+1)/2;
 	byteToWord(&rdid_32, rdid, 4*sizeof(uint8_t));
 	rdid_32 = rdid_32 >> ((4-rdid_size)*8);
+	printf("rdid = %u\n", rdid_32);
 	if (!rdid_32) {
 		// broadcast
-		routingTableGetBroadcast(routing_table, sg_serial->ip_addr);
+		//routingTableGetBroadcast(routing_table, sg_serial->ip_addr);
 	} else {
 		routingTableRDIDToIP(routing_table, rdid_32, sg_serial->ip_addr);
 	}
